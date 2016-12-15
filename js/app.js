c9d84ddb87bbaffe70fbe57a785232ec3abf44a1
William Cox <willubyu7@gmail.com>
2016-12-15 16:08:24 -0700

    working on route. unhandled rejection

--- js/app.js
+++ js/app.js
@@ -1,22 +1,32 @@
-var app = angular.module("devMtnApp", ["ngRoute"]);
-app.config(['$routeProvider', function($routeProvider) {
-  $routeProvider
-  .when("/", {
-    templateUrl: "views/init_view.html"
+angular.module("devMtnApp", ["ui.router"])
+.config(function($stateProvider, $urlRouterProvider) {
+
+  $urlRouterProvider.otherwise('/views');
+
+  $stateProvider
+  .state('views', {
+    url: '/views',
+    templateUrl: './views/init_view.html'
   })
-  .when("views/landing_view.html", {
-    templateUrl: "views/landing_view.html"
+  .state('landing', {
+    url: '/landing',
+    templateUrl: 'views/landing_view.html'
   })
-  .when("views/friends_view.html", {
-    templateUrl: "views/friends_view.html"
+  .state('friends', {
+    url: '/friends',
+    templateUrl: 'views/friends_view.html'
   })
-  .when("views/friends_profile_view.html", {
-    templateUrl: "views/friends_profile_view.html"
+  .state('fProfile', {
+    url: '/fProfile',
+    templateUrl: 'views/friends_profile_view.html'
   })
-  .when("views/init_view.html", {
-    templateUrl: "views/init_view.html"
+  .state('newFriends', {
+    url: '/newFriends',
+    templateUrl: 'views/newFriends_view.html'
   })
-  .when("view/newFriends_view.html", {
-    templateUrl: "view/newFriends_view.html"
+  .state('update', {
+    url: '/update',
+    templateUrl: 'views/update_profile_view.html'
   });
-}]);
+
+});
